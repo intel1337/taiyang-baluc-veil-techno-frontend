99c38fadc9ec112cbc2447092384699007321432
tae <tae@taes-MacBook-Air.local>
2025-09-29 10:48:49 +0200

    removed .git

---
+++ nest-app/frontend/src/app/landing/landing.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-landing',
+  imports: [RouterOutlet],
+  templateUrl: './landing.html',
+  styleUrl: './landing.css'
+})
+export class Landing {
+
+}
