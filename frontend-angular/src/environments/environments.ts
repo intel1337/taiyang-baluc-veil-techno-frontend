2f924ca756606d0e59c4c5c988fb826be9d392da
tae <tae@taes-MacBook-Air.local>
2025-10-01 15:30:28 +0200

    deploy

---
+++ frontend-angular/src/environments/environments.ts
@@ -0,0 +1,9 @@
+export const environment: {
+  appwriteEndpoint: string;
+  appwriteProjectId: string;
+  appwriteProjectName: string;
+} = {
+  appwriteEndpoint: 'https://fra.cloud.appwrite.io/v1',
+  appwriteProjectId: '68dd29e500106260b936',
+  appwriteProjectName: 'Kanban'
+};
